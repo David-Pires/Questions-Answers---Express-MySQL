751a58cd1d15b5723dd14e19b39434ab93e869cf
David Pires <developer.david@outlook.com>
2022-05-23 20:40:36 -0300

    fix: rebase commits

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
 //Sequelize('') passar o nome do banco que iremos nos conectar
-const connection = new Sequelize('guiaperguntas', 'root', '.', {
+const connection = new Sequelize('guiaperguntas', 'root', 'novoteste', {
   host: 'localhost',
   dialect: 'mysql'
 })
