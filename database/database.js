1606b159d2bec166d3e08f2bdb6d2098ee702480
David Pires WorkPlace <developer.david@outlook.com>
2022-05-25 11:24:34 -0300

    test: 2

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
 //Sequelize('') passar o nome do banco que iremos nos conectar
-const connection = new Sequelize('guiaperguntas', 'root', '1', {
+const connection = new Sequelize('guiaperguntas', 'root', '2', {
   host: 'localhost',
   dialect: 'mysql'
 })
